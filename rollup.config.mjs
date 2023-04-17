33112e319aefab1494964177e44e4a9dba33ab3b
patrik.hultgren <info@patrikhultgren.se>
2023-04-17 11:49:26 +0200

    Trying to use inline sourceMap

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,6 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import sourcemaps from 'rollup-plugin-sourcemaps'
 import dts from 'rollup-plugin-dts'
 
 import packageJson from './package.json' assert { type: 'json' }
@@ -14,12 +13,12 @@ export default [
       {
         file: packageJson.main,
         format: 'cjs',
-        sourcemap: true,
+        sourcemap: 'inline',
       },
       {
         file: packageJson.module,
         format: 'esm',
-        sourcemap: true,
+        sourcemap: 'inline',
       },
     ],
     plugins: [
@@ -27,7 +26,6 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
-      sourcemaps(),
     ],
     external: ['react'],
   },
