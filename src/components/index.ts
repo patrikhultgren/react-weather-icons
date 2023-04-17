8ccefa156e7a325824d3f99a31e7f8872780bc97
patrik.hultgren <info@patrikhultgren.se>
2023-04-17 13:41:37 +0200

    Add Cloudy and FairDay icons

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as ClearSkyDay } from './ClearSkyDay'
 export { default as ClearSkyNight } from './ClearSkyNight'
+export { default as Cloudy } from './Cloudy'
+export { default as FairDay } from './FairDay'
