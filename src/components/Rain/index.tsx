29234931809b74e60e139dc1c6c3efa8bea0a08a
patrik.hultgren <info@patrikhultgren.se>
2023-04-25 13:13:32 +0200

    Fix Rain

--- src/components/Rain/index.tsx
+++ src/components/Rain/index.tsx
@@ -7,7 +7,7 @@ type IProps = React.SVGProps<SVGSVGElement> & {
   y?: string | number
 }
 
-const PartlyCloudyNight = ({
+const Rain = ({
   className,
   x = 0,
   y = 0,
@@ -15,12 +15,9 @@ const PartlyCloudyNight = ({
   title,
   ...rest
 }: IProps) => {
+  const raindrop = useId()
   const cloud = useId()
-  const cloudMask = useId()
-  const moon = useId()
-  const moonGrad = useId()
-  const s04 = useId()
-  const s03n = useId()
+  const s09 = useId()
 
   return (
     <svg
@@ -35,6 +32,12 @@ const PartlyCloudyNight = ({
       {...rest}
     >
       {title && <title>{title}</title>}
+      <symbol id={raindrop}>
+        <path
+          fill="#0062bf"
+          d="M2.5,13A2.5,2.5,0,0,1,.21,9.51l3.55-8a2.5,2.5,0,0,1,4.57,2l-3.55,8A2.5,2.5,0,0,1,2.5,13Z"
+        ></path>
+      </symbol>
       <symbol id={cloud}>
         <path d="M55.7,5A23.94,23.94,0,0,0,34.37,18.05a9.9,9.9,0,0,0-12.78,5.56,15,15,0,0,0-1.71-.1A14.81,14.81,0,0,0,9.2,28,14.63,14.63,0,0,0,5,38.17v.21A14.83,14.83,0,0,0,19.88,53.06H75.59a14.3,14.3,0,0,0,3.67-28.14A23.93,23.93,0,0,0,55.7,5Z"></path>
         <image
@@ -45,60 +48,44 @@ const PartlyCloudyNight = ({
           xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFYAAAAkCAMAAAAkYj0PAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAVUExURSgoKExpcaCgoFBQUG5ublBQUISEhI1fsT0AAAAHdFJOUxsACBsPFRpGXuFgAAABWElEQVRIx7XV25bDIAgF0BMu/v8nF/E+iWlqHNKVN3cpIMXxL4GFM3SQfTazkUyxk63oLYwlVSy2silXkS/wUrZS2a3ZCn1zsdSw7UUYijuHsTa1IvfwWrbSXLkc4N9r27JViwmM1UtWXA3hohQ41m6vl8FQZi7wu2z7KXPW4uRiZS+2AmdXN7DdQEQWQHYHlt6z0dXBBa2xeeVktiZc1jDoF5eGkI4d4MjKc7cNbZ3bqjocLLx5oPDYTaIftcfvAvcs2GFxVsJTOP1wO1jGdUSLaz/DWA1Tl45+Tkqul2ArcPzayGq8JafOUffP3TUp6JQs+Rptc6vtmtBkUw+dv0NzWG0PYf8O7Ym09+ITXyXOPZqEX95aFe3PKxRsL2XV3HR+ZALirPSF0ceHp6F51WBv1A22VaW2GHWzWvat8LOAPf4CrjrA+neNK7+PQBf/DmmLrId09/QDWyESBsibwBUAAAAASUVORK5CYII="
         ></image>
       </symbol>
-      <symbol id={s04}>
+      <symbol id={s09}>
         <use
           xlinkHref={`#${cloud}`}
-          fill="#dddddd"
+          fill="#b2b2b2"
           x="0"
           y="0"
           width="100"
           height="100"
           transform="translate(3,18) scale(1,1)"
         ></use>
+        <use
+          xlinkHref={`#${raindrop}`}
+          x="0"
+          y="0"
+          width="100"
+          height="100"
+          transform="translate(32,78) scale(1,1)"
+        ></use>
+        <use
+          xlinkHref={`#${raindrop}`}
+          x="0"
+          y="0"
+          width="100"
+          height="100"
+          transform="translate(45,87) scale(1,1)"
+        ></use>
+        <use
+          xlinkHref={`#${raindrop}`}
+          x="0"
+          y="0"
+          width="100"
+          height="100"
+          transform="translate(60,78) scale(1,1)"
+        ></use>
       </symbol>
-      <symbol id={moon}>
-        <path
-          d="M28.43,0A28.44,28.44,0,0,1,32.3,14.32,28.61,28.61,0,0,1,3.69,42.93,28.71,28.71,0,0,1,0,42.66,28.59,28.59,0,1,0,28.43,0Z"
-          fill={`url(#${moonGrad})`}
-        ></path>
-      </symbol>
-      <defs>
-        <mask id={cloudMask}>
-          <rect x="0" y="0" width="100" height="100" fill="white"></rect>
-          <use
-            xlinkHref={`#${cloud}`}
-            fill="black"
-            stroke="black"
-            strokeLinejoin="round"
-            strokeWidth="10"
-            x="0"
-            y="0"
-            width="100"
-            height="100"
-            transform="translate(3,18) scale(1,1)"
-          ></use>
-        </mask>
-        <linearGradient id={moonGrad} x1="0%" y1="50%" x2="100%" y2="0%">
-          <stop offset="0%" stopColor="#686e73" />
-          <stop offset="100%" stopColor="#6a7075" />
-        </linearGradient>
-      </defs>
-      <symbol id={s03n}>
-        <g mask={`url(#${cloudMask})`}>
-          <use
-            xlinkHref={`#${moon}`}
-            x="0"
-            y="0"
-            width="100"
-            height="100"
-            transform="translate(2,5) scale(0.714285714,0.714285714)"
-          ></use>
-        </g>
-        <use xlinkHref={`#${s04}`} x="0" y="0" width="100" height="100"></use>
-      </symbol>
-      <use xlinkHref={`#${s03n}`} x="0" y="0" width="100" height="100"></use>
+      <use xlinkHref={`#${s09}`} x="0" y="0" width="100" height="100"></use>
     </svg>
   )
 }
 
-export default PartlyCloudyNight
+export default Rain
