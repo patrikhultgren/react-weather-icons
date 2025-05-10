477dd4a238cec1bf41fcb9b5a9f1af09e892292f
patrikhultgren <info@patrikhultgren.se>
2025-05-10 16:47:35 +0200

    Upgrade to React 19 and Storybook 8 (#1)
    * Upgrade to Storybook 8
    
    * Add React preset to Babel
    
    * Remove preview
    
    * Upgrade to React 19

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,32 @@
 import type { StorybookConfig } from '@storybook/react-webpack5'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-webpack5-compiler-babel',
+    '@chromatic-com/storybook',
   ],
+
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
   },
+
+  babel: async (options) => ({
+    ...options,
+    presets: [
+      ...(options.presets || []),
+      ['@babel/preset-react', { runtime: 'automatic' }],
+    ],
+  }),
 }
 export default config
